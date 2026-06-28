08a214ec86efb9359aaf4c0305a64da7fb2bfc87
“Eisrstary“ <2188558359@qq.com>
2026-06-29 00:25:09 +0800

    Refactor code structure for improved readability and maintainability

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod seed;
-pub mod params;
-pub mod generator;
-pub mod textify;
-pub mod ffi;
-
-pub use generator::{Bias, Generator, Personality};
-pub use params::{Domain, ParamDef, Range, PARAMS};
-pub use seed::Seed;
